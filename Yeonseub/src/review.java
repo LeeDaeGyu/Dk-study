6d9db51363d97b12d81b4df0b5dc575988f7664d
LeeDaeGyu <60695@naver.com>
2019-01-05 15:55:26 +0900

    Study review

---
+++ Yeonseub/src/review.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class review {
+	public static void main(String[] org) {
+//		Scanner scanner = new Scanner(System.in);
+//		System.out.println("출력하고 싶은 단을 입력하세요");
+//		int number = scanner.nextInt();	//이 부분이 입력이기 때문에 이 위에 print가 있어야된다
+//		System.out.println(number*1);
+//		System.out.println(number*2);
+//		System.out.println(number*3);
+//		System.out.println(number*4);
+//		System.out.println(number*5);
+//		System.out.println(number*6);
+//		System.out.println(number*7);
+//		System.out.println(number*8);
+//		System.out.println(number*9);
+		
+		
+	}
+}
